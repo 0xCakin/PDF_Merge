763721a76be88bdfab7167b18d9d34414d1e72a1
Caner Akin <Caner.Akin@ghd.com>
2021-10-01 14:37:12 -0400

    firt commit

---
+++ ReadME.md
@@ -0,0 +1,29 @@
+# PDF Merge
+Based on [PyPDF2 Library](https://anaconda.org/conda-forge/pypdf2/)，in [Python3] (https://www.python.org/downloads/).
+
+# Introduction
+
+This program will scan the given folder and generate a merged pdf file with the given name. 
+
+# Note
+Make sure that there is no space in the folder name.
+
+# How to use it
+
+- Go to dist folder and run `merge_PDF-3.exe` file
+- Download the corresponding Foxit Quick PDF Library version
+
+```python
+Folder path to PDFs that need merging: 
+#<enter the folder path> 
+#e.g.    
+
+Q:\GIS\PROJECTS\11213000s\11213985\Layouts\20211001_AdditionalSurveyExtentUpdate
+```
+```python
+What should I call the file?
+# it will add .pdf, so only type the filename without extension. 
+test
+
+# The file will be created in the given folder as "test.py"
+
---
+++ merge_PDF-3.py
@@ -0,0 +1,60 @@
+##################################################################################################################################
+# Author = Caner Akin                                                                                                            #
+# Date = October 01, 2021                                                                                                        #
+# Purpose = Merge PDFs from the given folder path                                                                                #
+# REF = Found in geektechstuff.com                                                                                               #
+# Requirements = 
+#       - Python3
+#       - PyPDF2 library:                                                                                                        #
+#           PIP - pip install PyPDF2                                                                                             #
+#           ANACONDA/CONDA - conda install -c conda-forge pypdf2                                                                 #
+# Direction = The code will ask for the related folder and export name. Make sure that the folder link doesn't have space.       #
+#       Example:                                                                                                                 #
+#           Folder path to PDFs that need merging: C:\example\Caner                                                              #
+#           What should I call the file? test_caner                                                                              # 
+##################################################################################################################################
+
+
+#Requires the “PyPDF2” and “OS” modules to be imported
+import os, PyPDF2
+def main():
+    #Ask user where the PDFs are
+    userpdflocation=input("Folder path to PDFs that need merging:")
+
+    #Sets the scripts working directory to the location of the PDFs
+    os.chdir(userpdflocation)
+
+    #Ask user for the name to save the file as
+    userfilename=input("What should I call the file?")
+
+    #Get all the PDF filenames
+    pdf2merge = []
+    for filename in os.listdir("."):
+        if filename.endswith(".pdf"):
+            pdf2merge.append(filename)
+
+    pdfWriter = PyPDF2.PdfFileWriter()
+
+    #loop through all PDFs
+    for filename in pdf2merge:
+
+    #rb for read binary
+        pdfFileObj = open(filename,"rb")
+        pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+    #Opening each page of the PDF
+        for pageNum in range(pdfReader.numPages):
+            pageObj = pdfReader.getPage(pageNum)
+            pdfWriter.addPage(pageObj)
+
+    #save PDF to file, wb for write binary
+    pdfOutput = open(userfilename+".pdf", "wb")
+
+    #Outputting the PDF
+    pdfWriter.write(pdfOutput)
+
+    #Closing the PDF writer
+    pdfOutput.close()
+
+if __name__ == "__main__":
+    main()
